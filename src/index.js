145a9872c1827e65397248660dc24f0182f13129
thanhphamtt <thanhpham1479@gmail.com>
2017-12-14 04:13:29 +0700

    up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import configureStore from './store/configureStore';
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
 require('./favicon.ico'); // Tell webpack to load favicon.ico
-import ImageApp from './containers/ImageApp'
+import ImageApp from './containers/ImageApp';
+import rootReducer from "./reducers/index";
+import initialState from "./reducers/initialState";
+import { createStore } from 'redux';
 
-const store = configureStore();
+let store = createStore(rootReducer,initialState);
 
 render(
   <Provider store={store}>
